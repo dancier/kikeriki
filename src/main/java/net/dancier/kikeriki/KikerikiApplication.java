8b9f4527f33e4871f507a41f090b3cacf270d1a8
Kai Moritz <kai@juplo.de>
2022-11-27 17:31:37 +0100

    Created an empty Spring Boot 3.0.0 Project
    * Maven
    * Java 17
    * JAR
    * Dependencies:
    ** Spring Boot Dev Tools
    ** Lombok
    ** Spring Configuration Processor
    ** Spring Web
    ** Spring for Apache Kafka
    ** Spring Boot Actuator
    ** Validation

---
+++ src/main/java/net/dancier/kikeriki/KikerikiApplication.java
@@ -0,0 +1,13 @@
+package net.dancier.kikeriki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KikerikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KikerikiApplication.class, args);
+	}
+
+}
