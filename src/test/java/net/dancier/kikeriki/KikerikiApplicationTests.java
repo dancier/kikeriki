a8ae415854fc6d7402b2e62237bbab421b8cf380
Kai Moritz <kai@juplo.de>
2022-11-27 17:31:37 +0100

    Created an empty Spring Boot 3.0.0 Project
    * Gradle
    * Java 17
    * JAR
    * Dependencies:
    ** Spring Boot Dev Tools
    ** Lombok
    ** Spring Configuration Processor
    ** Spring Web
    ** Spring for Apache Kafka
    ** Spring Boot Actuator
    ** Validation

---
+++ src/test/java/net/dancier/kikeriki/KikerikiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dancier.kikeriki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KikerikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
