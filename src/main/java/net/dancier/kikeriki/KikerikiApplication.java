a2e5c4048261caa3993aac84d53e26055c457b3d
Marc Gorzala <marc@dancier.net>
2023-12-28 14:03:00 +0100

    enable scheduling

--- src/main/java/net/dancier/kikeriki/KikerikiApplication.java
+++ src/main/java/net/dancier/kikeriki/KikerikiApplication.java
@@ -3,7 +3,9 @@ package net.dancier.kikeriki;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @Slf4j
 public class KikerikiApplication
