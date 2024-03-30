3755d1a8015c80a504c20fdb435c9899c6c91e7e
Marc Gorzala <marc@dancier.net>
2024-03-30 11:42:09 +0100

    lock and list

--- src/main/java/net/dancier/kikeriki/adapter/out/infomail/InfoMailCheckJob.java
+++ src/main/java/net/dancier/kikeriki/adapter/out/infomail/InfoMailCheckJob.java
@@ -1,16 +1,25 @@
 package net.dancier.kikeriki.adapter.out.infomail;
 
+import lombok.RequiredArgsConstructor;
+import net.dancier.kikeriki.adapter.out.mail.MailOutboxJpaEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+
 @Component
+@RequiredArgsConstructor
 public class InfoMailCheckJob {
   private static final Logger log = LoggerFactory.getLogger(InfoMailCheckJob.class);
+
+  private final ScheduledInfoMailCheckJpaRepository scheduledInfoMailCheckJpaRepository;
 
   @Scheduled(fixedRate = 5000L)
   public void check() {
     log.info("checking");
+    Collection<MailOutboxJpaEntity> mailOutboxJpaEntities = scheduledInfoMailCheckJpaRepository.lockAndList();
+    log.info("Got: " + mailOutboxJpaEntities.size());
   }
 }
