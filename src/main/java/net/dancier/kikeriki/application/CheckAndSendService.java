943a7ea37c780a7e64a13470aefd56d10cde1b6f
Marc Gorzala <marc@dancier.net>
2024-03-31 15:26:08 +0200

    thymeleaf

--- src/main/java/net/dancier/kikeriki/application/CheckAndSendService.java
+++ src/main/java/net/dancier/kikeriki/application/CheckAndSendService.java
@@ -4,20 +4,25 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.dancier.kikeriki.adapter.out.userinfo.UserInfoAdapter;
 import net.dancier.kikeriki.application.domain.model.messages.EmailSendingRequestedCommand;
+import net.dancier.kikeriki.application.domain.model.state.InfoMail;
 import net.dancier.kikeriki.application.domain.model.state.State;
 import net.dancier.kikeriki.application.port.StatePort;
+import net.dancier.kikeriki.application.port.UserInfoDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
 public class CheckAndSendService {
-  private final Logger log = LoggerFactory.getLogger(CheckAndSendService.class);
+  private final static Logger log = LoggerFactory.getLogger(CheckAndSendService.class);
+  public final static String USER_INFO_MAIL = "text/user-info";
 
   private final StatePort statePort;
 
@@ -25,26 +30,37 @@ public class CheckAndSendService {
 
   private final ApplicationEventPublisher applicationEventPublisher;
 
+  private final TemplateEngine templateEngine;
+
   @Transactional
   public void checkAndSend(String dancerId) {
     log.info("CheckAndSend for {}", dancerId);
     State state = statePort.get(dancerId);
     if (state.isCandidateForSendingMail(LocalDateTime.now())) {
-      sendMail(dancerId);
+      sendMail(dancerId, state);
       statePort.save(state.toDto(), dancerId);
     }
   }
-  public void sendMail(String dancerId) {
+  public void sendMail(String dancerId, State state) {
     log.info("Sending InfoMail...");
-    String mailAddress = userInfoAdapter.loadByDancerId(dancerId).getEmailAddress();
+    UserInfoDto userInfoDto =  userInfoAdapter.loadByDancerId(dancerId);
+    String emailAddress = userInfoDto.getEmailAddress();
     EmailSendingRequestedCommand emailSendingRequestedCommand =
       new EmailSendingRequestedCommand.EmailSendingRequestedCommandBuilder()
         .setFrom("marc@gorzala.de")
         .setId(UUID.randomUUID().toString())
-        .setTo(new String[]{mailAddress})
-        .setSubject("Infomail")
-        .setText("Hallo Welt")
+        .setTo(new String[]{emailAddress})
+        .setSubject("Neues für Dich auf Dancier.net")
+        .setText(createBody(state, userInfoDto))
         .build();
     applicationEventPublisher.publishEvent(emailSendingRequestedCommand);
+    state.setLastMailMessage(InfoMail.of(LocalDateTime.now()));
+  }
+
+  private String createBody(State state, UserInfoDto userInfoDto) {
+    Map<String, Object> map = new HashMap<>();
+    map.put("unread_messages", 10);
+    Context context = new Context(Locale.GERMANY, map);
+    return templateEngine.process(USER_INFO_MAIL, context);
   }
 }
